9a0bd3a5bb8e5d5cbf369fadf57a99b364daf61b
Will Dickson <wbd@caltech.edu>
2019-08-26 17:54:57 -0700

    Added to examples.

--- examples/find_arena.py
+++ examples/find_arena.py
@@ -1,4 +1,6 @@
 import  turntable_utilities
+import numpy as np
+import cv2
 
 filename = 'data/IMG_0957.mp4'
 results = turntable_utilities.arena_tools.find_arena(filename)
@@ -8,3 +10,16 @@ mask_gray = results['mask_gray']
 mask_bgr = results['mask_bgr']
 img_max = results['img_max']
 img_w_contour = results['img_w_contour']
+
+test_img_gray = 255*np.ones(mask_gray.shape,dtype=np.uint8)
+test_img_gray[mask_gray] = 0
+
+
+test_img_bgr = 255*np.ones(mask_bgr.shape,dtype=np.uint8)
+test_img_bgr[mask_bgr] = 0
+
+cv2.imshow('img_max', img_max)
+cv2.imshow('mask_bgr', test_img_bgr)
+cv2.imshow('mask_gray', test_img_gray)
+cv2.imshow('img_w_contour', img_w_contour)
+cv2.waitKey(-1)
--- examples/find_fly.py
+++ examples/find_fly.py
@@ -3,3 +3,6 @@ import turntable_utilities
 filename = 'data/IMG_0957.mp4'
 arena_dict = turntable_utilities.arena_tools.find_arena(filename, bndry_threshold=200, erode_kernel_size=13)
 fly_results = turntable_utilities.fly_tools.find_fly_in_video(filename, arena_dict, threshold=127, display=True)
+
+
+
